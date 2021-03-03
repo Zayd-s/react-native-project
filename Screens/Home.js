b18ff2ad23513efff04d7ad4b1d0a26a4cf8da4c
Zayd-s <zaydsabeh@hotmail.com>
2021-03-03 20:31:54 +0000

    Add complete stack navigation to Home.js

--- Screens/Home.js
+++ Screens/Home.js
@@ -6,8 +6,14 @@ class HomeScreen extends Component{
     const navigation = this.props.navigation;
 
     return(
-    <SafeAreaView style={ styles.container}>
-      <ScrollView horizontal style={ styles.HorizontalScroll }>
+    
+    <SafeAreaView style={styles.container}>
+
+    <SafeAreaView style={ styles.HorizontalScroll}>
+      <Text style={styles.Pic1Title}>
+        Top-rated Cafés
+      </Text>
+      <ScrollView horizontal>
 
         <TouchableOpacity
           onPress={() => navigation.navigate('Location1')}>
@@ -39,54 +45,89 @@ class HomeScreen extends Component{
             source={{ uri: 'https://cdn.shopify.com/s/files/1/2271/1033/files/105598293_4583241001701599_9188415003459821105_o.jpg?v=1598446566'}}/>
         </TouchableOpacity>
 
-     </ScrollView>
-     
-     
+      </ScrollView>
+    </SafeAreaView>
+
+    <SafeAreaView style={ styles.HorizontalScroll}>
+      <Text style={styles.Pic1Title}>
+        New Cafés
+      </Text>
+      <ScrollView horizontal>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Location2')}>
+          <Text style={{alignSelf: "center"}}>
+          Café Crafts
+          </Text>
+          <Image style={ styles.Shop2 }
+            resizeMode="cover"
+            source={{ uri: 'https://www.comunicaffe.com/wp-content/uploads/2018/11/Starbucks-New-York.jpg'}}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Location1')}>
+          <Text style={{alignSelf: "center"}}>
+          Paterissie Julie
+          </Text>
+          <Image style={ styles.Shop1 }
+            resizeMode="cover"
+            source={{ uri: 'https://static.dezeen.com/uploads/2017/08/daodaocoffee-HAD-architects-and-EPOS-interiors_dezeen_hero-3.jpg'}}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Location3')}>
+          <Text style={{alignSelf: "center"}}>
+          Hot Cups
+          </Text>
+          <Image style={ styles.Shop3 }
+            resizeMode="cover"
+            source={{ uri: 'https://cdn.shopify.com/s/files/1/2271/1033/files/105598293_4583241001701599_9188415003459821105_o.jpg?v=1598446566'}}/>
+        </TouchableOpacity>
+
+      </ScrollView>
+    </SafeAreaView>
 
 
-    </SafeAreaView>)
+
+    </SafeAreaView>
+
+    )
   }
 }
 export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.4,
-    //justifyContent: "center",
-    //alignItems: "center",
-    //paddingHorizontal: 10
-    //backgroundColor: 'blue',
-    //width: 250
+    flex: 1,
+    //paddingTop: 50,
+
+  },
+  Pic1Title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    alignSelf: "center",
+    top: -10,
+
   },
   HorizontalScroll:{
     marginTop: 40,
-    //marginBot: 50,
-    //flex: 1,
-    //paddingLeft: 50,
+    flex: 0.4,
   },
   Shop1: {
     width: 150,
     height: 150,
-    //flex: 0.2,
-    //alignSelf: "center",
-    //top: 100,
-    //left: 30,
     borderRadius: 15,
     margin: 10,
   },
   Shop2: {
     width: 150,
     height: 150,
-    //top: 100,
-    //left: 50,
     borderRadius: 15,
     margin: 10,
   },
   Shop3: {
     width: 150,
     height: 150,
-    //top: 100,
-    //left: 70,
     borderRadius: 15,
     margin: 10,
   }
